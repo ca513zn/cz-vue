8777ec854e1f167ca3541bfa0d13f9f07f6ce583
czuniga <czuniga@tibco-support.com>
2020-08-05 18:38:37 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <Header msg="Welcome to Your Vue.js App" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox v-if="questions.length" :currQuestion="questions[index]" :next="next" />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    }
+  },
+
+  mounted: async function() {
+    const request = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple"
+    );
+    const data = await request.json();
+    this.questions = data.results;
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="questionBox">
+    <b-jumbotron>
+      <template v-slot:lead>{{currQuestion.question}}</template>
+
+      <hr class="my-4" />
+
+      <b-list-group class="questionBox__listGroup">
+        <b-list-group-item
+          v-for="(answer, idx) in answers"
+          :key="idx"
+          @click="selectAnwser(idx)"
+        >{{answer}}</b-list-group-item>
+      </b-list-group>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button class="questionBox__next" variant="success" @click="next">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    currQuestion: Object,
+    next: Function
+  },
+  data() {
+    return {
+      selectedIdx: null
+    };
+  },
+  methods: {
+    selectedAnswer(idx) {}
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currQuestion.incorrect_answers];
+      answers.push(this.currQuestion.correct_answer);
+      return answers;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+.questionBox__listGroup {
+  margin-bottom: 15px;
+}
+.questionBox__next {
+  margin: 0 5px;
+}
+</style>
